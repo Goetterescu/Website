9f3d4e95e5f54673a8245906ba0aeba26b64663a
Goetterescu <daniel.goetter@gmx.de>
2021-11-08 19:31:46 +0100

    edited -fish.py

--- Sneaks and tests/-fish.py
+++ Sneaks and tests/-fish.py
@@ -14,48 +14,49 @@ if a[0] == "-fish":
         
         # Here comes the main decision
         print(ranfish) # Debug
-        if ranfish <= 30: pass # Short-hand-if because we are cool
+        if ranfish <= 30: loot = "x" # Short-hand-if because we are cool
         #common
-        if ranfish >= 31 and ranfish <= 70: pass
+        if ranfish >= 31 and ranfish <= 70: loot = "x"
         #well-known
-        if ranfish >= 71 and ranfish <= 100: pass
-        if ranfish >= 101 and ranfish <= 130: pass
+        if ranfish >= 71 and ranfish <= 100: loot = "x"
+        if ranfish >= 101 and ranfish <= 130: loot = "x"
         #known
-        if ranfish >= 131 and ranfish <= 150: pass
-        if ranfish >= 151 and ranfish <= 170: pass
+        if ranfish >= 131 and ranfish <= 150: loot = "x"
+        if ranfish >= 151 and ranfish <= 170: loot = "x"
         #fancy
-        if ranfish >= 171 and ranfish <= 180: pass
-        if ranfish >= 181 and ranfish <= 190: pass
-        if ranfish >= 191 and ranfish <= 200: pass
-        if ranfish >= 201 and ranfish <= 210: pass
-        if ranfish >= 211 and ranfish <= 220: pass
-        if ranfish >= 221 and ranfish <= 230: pass
-        if ranfish >= 231 and ranfish <= 240: pass
-        if ranfish >= 241 and ranfish <= 250: pass
+        if ranfish >= 171 and ranfish <= 180: loot = "x"
+        if ranfish >= 181 and ranfish <= 190: loot = "x"
+        if ranfish >= 191 and ranfish <= 200: loot = "x"
+        if ranfish >= 201 and ranfish <= 210: loot = "x"
+        if ranfish >= 211 and ranfish <= 220: loot = "x"
+        if ranfish >= 221 and ranfish <= 230: loot = "x"
+        if ranfish >= 231 and ranfish <= 240: loot = "x"
+        if ranfish >= 241 and ranfish <= 250: loot = "x"
         #rare
-        if ranfish >= 251 and ranfish <= 255: pass
-        if ranfish >= 256 and ranfish <= 260: pass
-        if ranfish >= 261 and ranfish <= 265: pass
-        if ranfish >= 266 and ranfish <= 270: pass
-        if ranfish >= 271 and ranfish <= 275: pass
+        if ranfish >= 251 and ranfish <= 255: loot = "x"
+        if ranfish >= 256 and ranfish <= 260: loot = "x"
+        if ranfish >= 261 and ranfish <= 265: loot = "x"
+        if ranfish >= 266 and ranfish <= 270: loot = "x"
+        if ranfish >= 271 and ranfish <= 275: loot = "x"
         #very-rare
-        if ranfish >= 276 and ranfish <= 278: pass
-        if ranfish >= 279 and ranfish <= 281: pass
-        if ranfish >= 282 and ranfish <= 284: pass
-        if ranfish >= 285 and ranfish <= 287: pass
+        if ranfish >= 276 and ranfish <= 278: loot = "x"
+        if ranfish >= 279 and ranfish <= 281: loot = "x"
+        if ranfish >= 282 and ranfish <= 284: loot = "x"
+        if ranfish >= 285 and ranfish <= 287: loot = "x"
         #unknown
-        if ranfish >= 288 and ranfish <= 289: pass
-        if ranfish >= 290 and ranfish <= 291: pass
-        if ranfish >= 292 and ranfish <= 293: pass
+        if ranfish >= 288 and ranfish <= 289: loot = "x"
+        if ranfish >= 290 and ranfish <= 291: loot = "x"
+        if ranfish >= 292 and ranfish <= 293: loot = "x"
         #unique
-        if ranfish == 294: pass
-        if ranfish == 295: pass
-        if ranfish == 296: pass
-        if ranfish == 297: pass
+        if ranfish == 294: loot = "x"
+        if ranfish == 295: loot = "x"
+        if ranfish == 296: loot = "x"
+        if ranfish == 297: loot = "x"
         #cursed
-        if ranfish >= 298 and ranfish <= 299: pass
-        if ranfish == 300: pass
+        if ranfish >= 298 and ranfish <= 299: loot = "x"
+        if ranfish == 300: loot = "x"
 
+        print("xuserx reeled in " + str(loot) + "!")
 
 #return
 
