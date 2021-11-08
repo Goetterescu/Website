09ce469e2bd11a06f93768e0f8ee66aaa8953708
Goetterescu <daniel.goetter@gmx.de>
2021-11-08 17:19:51 +0100

    Added Fishing contest results and changed the fish command

--- Sneaks and tests/-fish.py
+++ Sneaks and tests/-fish.py
@@ -1,17 +1,30 @@
-import random
+import random 
 
-i = random.randint(1, 12)
-print (i)
+       if a[0] == "-fish":
+            ranfish = random.randint(1,34)
+            if ranfish<=5:
+                self.__sendChat_delayed("%s reeled in some junk..." % self.sm.getUsernameColoured(source))
+            elif ranfish>6 and ranfish<=10:
+                self.__sendChat_delayed("%s reeled in a small tuna" % self.sm.getUsernameColoured(source))
+            elif ranfish>11 and ranfish<=14:
+                self.__sendChat_delayed("%s reeled in an squid" % self.sm.getUsernameColoured(source))
+            elif ranfish>15 and ranfish<=18:
+                self.__sendChat_delayed("%s reeled in an largemouth bass" % self.sm.getUsernameColoured(source))
+            elif ranfish>19 and ranfish<=24:
+                self.__sendChat_delayed("%s reeled in an rainbow trout" % self.sm.getUsernameColoured(source))
+            elif ranfish>25 and ranfish<=28:
+                self.__sendChat_delayed("%s reeled in a catfish" % self.sm.getUsernameColoured(source))
+            elif ranfish>29 and ranfish<=31:
+                self.__sendChat_delayed("%s reeled in an unknown fish!" % self.sm.getUsernameColoured(source))
+            elif ranfish>32 and ranfish<=33:
+                self.__sendChat_delayed("%s reeled in a fishing prize medal from the 60s!" % self.sm.getUsernameColoured(source))
+            elif ranfish==34:
+                self.__sendChat_delayed("%s reeled in a quest scroll!!!" % self.sm.getUsernameColoured(source))
+                
+            return
 
-if i >= 1 and i <= 3:
-	print ("O one")
-elif i >= 4 and i <= 5:
-	print ("O two")
-elif i >= 6 and i <= 8:
-	print ("O three")
-elif i >= 9 and i <= 11:
-	print ("O four")
-elif i == 12:
-	print ("O five")
-else:
-	print ("Error!")
+
+
+# a is the chat array. a[0] inital, a[1] first parameter and so on
+# self.__sendChat_delayed("%s reeled in a quest scroll!!!" % self.sm.getUsernameColoured(source))
+# [username] reeled [first word of loot] in:#bot during:[date of event]     this can be used to search the chat log after the event
